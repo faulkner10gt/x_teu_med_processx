67feccd460715a4831159236d61a66244ae040eb
System Administrator <admin@example.com>
2019-05-30 23:07:54 +0000

    Changes by 'System Administrator' on 'Med ProcessX – 1.0.0' -- Build #1

---
+++ sn/Med ProcessX/Dictionary Entry/x_teu_med_processx_medical_supplies/active_true/med_group/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : Med ProcessX
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-05-30 23:04:10
+ * Created By  : admin
+ * Updated On  : 2019-05-30 23:04:10
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=64e64c30dbb5f700e87dd49fdf9619e4
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
